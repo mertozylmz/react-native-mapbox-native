28ea5d233dc62a49cf35975ac2e49ba1b6dea28a
Mert Ozyilmaz <mertozyilmazz@gmail.com>
2019-10-07 17:41:12 +0300

    Now for ios added functions

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-import { requireNativeComponent } from 'react-native';
+import { requireNativeComponent, NativeModules, Platform } from 'react-native';
 
 const MapboxNative = requireNativeComponent('MapboxNative', null);
+
+export const AddPoint = (coord) => {
+    if (Platform.OS == 'ios') {
+        NativeModules.MapboxNative.addPoint(coord, true, () => {});
+    }
+}
+
+export const SetCoordinates = (coords) => {
+    if (Platform.OS == 'ios') {
+        NativeModules.MapboxNative.setCoordinates(coords);
+    }
+}
+
+export const StartNavigation = () => {
+    if (Platform.OS == 'ios') {
+        NativeModules.MapboxNative.startNavigation();
+    }
+}
+
+export const StopNavigation = () => {
+    if (Platform.OS == 'ios') {
+        NativeModules.MapboxNative.stopNavigation();
+    }
+}
+
+export const ClearMap = () => {
+    if (Platform.OS == 'ios') {
+        NativeModules.MapboxNative.clearMapItems();
+    }
+}
 
 export default MapboxNative;
